717b582615bd3d81ea73bf7d833f6248bceed4c5
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2014-06-02 21:08:29 -0700

    tests for fixity_checker.compare_sightings

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import shutil
 import os
 
 # 
-class TestCheck(unittest.TestCase):
+class TestCommand(unittest.TestCase):
 
     def setUp(self):
         self.workspace = tempfile.mkdtemp(prefix='yafixity-test-')
@@ -24,3 +24,20 @@ class TestCheck(unittest.TestCase):
         argv.update = None
         pp(argv)
         fixity_checker.main(argv)
+
+class TestCompare(unittest.TestCase):
+    def test_compare(self):
+        sight = fixity_checker.compare_sightings
+        a1 = { 'size': 1 }
+        a2 = { 'size': 2 }
+        b = { 'size': 1, 'md5': 'xyz' } # pretend we just saw this <--
+        bb = { 'size': 1, 'md5': 'xyz', 'path': '/path/' }
+        c = { 'size': 1, 'md5': 'abc' }
+        assert(sight(b,b))          # no change since last time
+        assert(sight(b,bb))         # no change since last time (with path in the database)
+        assert(sight(b,a1))         # have seen this file, but not this checksum type
+        assert(not(sight(b,a2)))    # new sighting with changed size
+        assert(not(sight(b,c)))     # new sighting with changed checksum
+
+class TestObserve(unittest.TestCase):
+    pass
