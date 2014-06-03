1ca5627508a53e3db82edaf5838023ba742d9b13
Brian Tingle <brian.tingle@ucop.edu>
2014-06-02 19:02:33 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+description = \
+"""
+Yet another fixity checker
+"""
+
+# for older pythons ...
+requirements = []
+try:
+    import hashlib
+except:
+    requirements.append("hashlib")
+
+
+requirements.append("shove")
+
+setup(
+    name = 'fixity_checker',
+    version= '0.1.0',
+    maintainer = "Brian Tingle",
+    maintainer_email = 'brian.tingle.cdlib.org@gmail.com',
+    packages = find_packages(),
+    install_requires = requirements,
+    py_modules = ['fixity_checker',],
+    entry_points = {
+        'console_scripts': [
+            'checker = fixity_checker:main',
+        ]
+    },
+    description = description,
+    test_suite = 'test',
+)
