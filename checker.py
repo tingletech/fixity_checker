dba13ef527f417f6933aa665be1987b7642e79b3
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2014-06-07 18:30:51 -0700

    daemon is basically working

--- checker.py
+++ checker.py
@@ -7,7 +7,6 @@ server daemon
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 import daemonocle
-import fixity_checker
 import argparse
 import pkg_resources  # part of setuptools
 from appdirs import user_data_dir
@@ -20,6 +19,7 @@ from collections import namedtuple
 import logging
 from shove import Shove
 import hashlib
+import psutil
 
 # raw_import() in 2 -> import() in 3
 try:
@@ -44,10 +44,6 @@ except KeyError:
 
 # read version out of setup.py
 __version__ = pkg_resources.require(APP_NAME)[0].version
-
-def cb_shutdown(message, code):
-    logging.info('Daemon is stopping')
-    logging.debug(message)
 
 
 def main(argv=None):
@@ -116,10 +112,8 @@ def main(argv=None):
 
     def main_loop():
         # nested here to gain access to `conf`
-        logging.basicConfig(
-            filename=conf.daemon.log,
-            level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s',
-        )
+
+        log_nice(conf)
         logging.info('Daemon is starting')
         while True:
             logging.debug('Still running')
@@ -139,9 +133,20 @@ def main(argv=None):
     else:
         return argv.func(conf, daemon)
 
+
+def cb_shutdown(message, code):
+    logging.info('Daemon is stopping')
+    logging.debug(message)
+
+
 def checker(conf):
+    """ the main loop """
+    MIN_LOOP = 15
+    startLoopTime = time.time()
     # persisted dict interface for long term memory
     observations = Shove(conf.data['data_url'], protocol=2)
+
+    # counts
 
     # for each hash type
     for hashtype in conf.data['hashlib']:
@@ -150,15 +155,25 @@ def checker(conf):
         for filepath in conf.data['archive_directories']:
             assert filepath, "arguments can't be empty"
             filepath = filepath+''  # http://fomori.org/blog/?p=486
-            fixity_checker.check_one_arg(filepath, observations, hashtype, False)
 
-    logging.info('looking for missing files')
+            check_one_arg(filepath, observations, hashtype, False)
+
     # check for missing files
+    logging.info('looking for missing files')
     for ____, value in list(observations.items()):
         assert os.path.isfile(value['path']),\
             "{} no longer exists or is not a file".format(value['path'])
 
     observations.close()
+
+    # output json reports
+    elapsedLoopTime = time.time() - startLoopTime
+    logging.info("elapsedLoopTime {0} {1} files {2} bytes".format(elapsedLoopTime))
+
+    if elapsedLoopTime < MIN_LOOP:
+        nap = MIN_LOOP - elapsedLoopTime
+        logging.info('sleeping for {0}'.format(nap))
+        time.sleep(nap)
 
 
 # ⌦
@@ -182,7 +197,7 @@ def init(args):
     if args.asset_directory:
         print("non interactive")
         directories = [os.path.abspath(x) for x in args.asset_directory]
-        _init(conf, directories, data_url_default, 'sha512', 'INFO')
+        _init(conf, directories, data_url_default, 'sha512')
         exit(0)
 
     # **
@@ -215,7 +230,7 @@ def init(args):
     )
     print(hash)
 
-    _init(conf, directories, data_url, hash, loglevel)
+    _init(conf, directories, data_url, hash)
 
 
 def _init(conf, directories, data_url, hash):
@@ -225,7 +240,8 @@ def _init(conf, directories, data_url, hash):
         'archive_directories': directories,
         'data_url': data_url,
         'hashlib': [ hash ],
-        'loglevel': 'INFO'
+        'loglevel': 'INFO',
+        'sleepiness': 1
     }
     os.mkdir(conf)
     os.mkdir(os.path.join(conf, 'logs'))
@@ -395,5 +411,204 @@ def valid_path(string):
         print('directory of file must exist')
 
 
+# ⌦
+# ⌦  file checking functions
+# ⌦
+
+
+def check_one_arg(filein, observations, hash, update, conf):
+    """check if the arg is a file or directory, walk directory for files"""
+    if os.path.isdir(filein):
+        for root, ____, files in os.walk(filein):
+            for f in files:
+                fullpath = os.path.join(root, f)
+                check_one_file(fullpath, observations, hash, update, conf)
+    else:
+        check_one_file(filein, observations, hash, update, conf)
+
+
+def check_one_file(filein, observations, hash, update, conf):
+    """check file one file against our memories"""
+    startTime = time.time()
+    filename = os.path.abspath(filein)
+    logging.info('{0}'.format(filename))
+    # normalize filename, take hash for key
+    filename_key = hashlib.sha224(filename.encode('utf-8')).hexdigest()
+    logging.debug('sha224 of path {0}'.format(filename_key))
+
+    seen_now = analyze_file(filename, hash)
+    logging.debug('seen_now {0}'.format(seen_now))
+
+    if filename_key in observations and not update:
+        # make sure things match
+        news = {}
+        looks_the_same = compare_sightings(
+            seen_now, observations[filename_key], news
+        )
+        assert bool(looks_the_same), "%r has changed" % filename
+        if any(news):
+            update = observations[filename_key]
+            update.update(news)
+            observations[filename_key] = update
+            observations.sync()
+            logging.debug('new memory {0}'.format(memory))
+    else:
+        # update observations
+        observations[filename_key] = seen_now
+        observations.sync()
+        logging.info('update observations')
+
+    elapsedTime = time.time() - startTime
+    time.sleep(os.getloadavg()[0] * elapsedTime * conf.data['sleepiness'])
+
+
+def compare_sightings(now, before, news={}):
+    """ return False if sightings differ, otherwise True """
+    logging.debug('now {0} before {1}'.format(now, before))
+    if not now['size'] == before['size']:
+        logging.error('sizes do not match')
+        return False
+    for check in list(now.keys()):
+        if check in before:
+            if now[check] != before[check]:
+                logging.error('{2} differ before:{1} now:{0}'.format(
+                    now[check], before[check], check)
+                )
+                return False
+        else:
+            logging.info('{0} not seen before for this'.format(check))
+            news[check] = now[check]
+    return True
+
+
+def analyze_file(filename, hash):
+    """ returns a dict of hash and size in bytes """
+    # http://www.pythoncentral.io/hashing-files-with-python/
+    hasher = hashlib.new(hash)
+    BLOCKSIZE = 1024 * hasher.block_size
+    with open(filename, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(BLOCKSIZE)
+        # in the future, add os.POSIX_FADV_DONTNEED support
+        # needs python 3 and newer linux kernel
+        # http://www.gossamer-threads.com/lists/python/python/1063241
+    return {
+        'size': os.path.getsize(filename),
+        hasher.name: hasher.hexdigest(),
+        'path': filename
+    }
+
+
+def log_nice(conf):
+    """ some ugly incantations to run nice and set up logging"""
+    # set debugging level
+    numeric_level = getattr(logging, conf.data['loglevel'], None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+
+    logging.basicConfig(
+        filename=conf.daemon.log,
+        level=numeric_level,
+        format='%(asctime)s [%(levelname)s] %(message)s',
+    )
+
+    # ionice... http://stackoverflow.com/a/6245160/1763984
+    p = psutil.Process(os.getpid())
+    # ... if we can http://stackoverflow.com/a/34472/1763984
+    if hasattr(p, 'set_ionice'):
+        p.set_ionice(psutil.IOPRIO_CLASS_IDLE)
+
+    os.nice(10)
+
+
+def fixity_checker_report_command(argv=None):
+    """ main for report generator """
+    parser = argparse.ArgumentParser(description=''.join(['Yet another fixity checker ', __version__]))
+    parser.add_argument('outputdir', nargs=1, help='file or directory',)
+    parser.add_argument('--data_url',
+                        help='database URL to shove to (file://... for files)',
+                        default=DATA)
+    parser.add_argument('--loglevel', default='ERROR')
+
+    if argv is None:
+        argv = parser.parse_args()
+
+    nice_log(argv.loglevel.upper())
+    # persisted dict interface for long term memory
+    observations = Shove(argv.data_url, protocol=PICKLE_PROTOCOL, flag='r')
+
+    fixity_checker_report(observations, argv.outputdir[0])
+
+    observations.close()
+
+
+def fixity_checker_report(observations, outputdir):
+    """ output a listing of our memories """
+    logging.debug("{0}, {1}".format(observations, outputdir))
+    shards = defaultdict(dict)
+    _mkdir(outputdir)
+    # sort into bins for transport
+    for key, value in list(observations.items()):
+        shard_key = key[:2]
+        shards[shard_key].update({key: value})
+    # write out json for each bin
+    for key, value in list(shards.items()):
+        out = os.path.join(outputdir, ''.join([key, '.json']))
+        with open(out, 'w') as outfile:
+            json.dump(shards[key], outfile, sort_keys=True,
+                      indent=4, separators=(',', ': '))
+
+
+def _mkdir(newdir):
+    """works the way a good mkdir should :)
+        - already exists, silently complete
+        - regular file in the way, raise an exception
+        - parent directory(ies) does not exist, make them as well
+    """  # http://code.activestate.com/recipes/82465-a-friendly-mkdir/
+    if os.path.isdir(newdir):
+        pass
+    elif os.path.isfile(newdir):
+        raise OSError("a file with the same name as the desired "
+                      "dir, '%s', already exists." % newdir)
+    else:
+        head, tail = os.path.split(newdir)
+        if head and not os.path.isdir(head):
+            _mkdir(head)
+        # print "_mkdir %s" % repr(newdir)
+        if tail:
+            os.mkdir(newdir)
+
+
 if __name__ == "__main__":
     sys.exit(main())
+"""
+Copyright © 2014, Regents of the University of California
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+- Neither the name of the University of California nor the names of its
+  contributors may be used to endorse or promote products derived from this
+  software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+"""
+
