ebc2a66c4e4f2f4fda5e9497f4591aab9e17b218
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2014-06-14 13:59:32 -0700

    gh-pages script worked

--- gen_push_doc.sh
+++ gen_push_doc.sh
@@ -3,5 +3,5 @@ set -eu
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) # http://stackoverflow.com/questions/59895
 cd "$DIR"
 docco fixity_checker.py
-mv docs/fixity_checker.py docs/index.html
+mv docs/fixity_checker.html docs/index.html
 git subtree push --prefix docs origin gh-pages
